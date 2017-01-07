d83d60c672b2b4995101f1444efc6be8de6a9214
Igor <Igor.dudschenko@rwth-aachen.de>
2017-01-07 17:52:01 +0100

    Change (initialInterval): Since the Hint is babbling about Local types we are casting variables to Local now

--- Ex10/src/de/rwth/i2/spa/IntervalDomain.java
+++ Ex10/src/de/rwth/i2/spa/IntervalDomain.java
@@ -5,6 +5,7 @@ import java.util.TreeMap;
 import java.util.Map.Entry;
 import java.util.function.BiConsumer;
 
+import soot.Local;
 import soot.Value;
 
 /*
@@ -12,55 +13,53 @@ import soot.Value;
  */
 public class IntervalDomain {
 
-	public TreeMap<String,Interval> delta;
+	public TreeMap<String, Interval> delta;
 
 	public IntervalDomain(Set<Value> variables, Interval initialInterval) {
 
 		/*
-		 * TODO
-		 * create new domain element over the given set of variables.
-		 * Hint: In Soot variables correspond to values of type Local.
+		 * TODO create new domain element over the given set of variables. Hint:
+		 * In Soot variables correspond to values of type Local.
 		 */
-		delta = new TreeMap<String,Interval>();
+		delta = new TreeMap<String, Interval>();
 		for (Value var : variables) {
-			delta.put(var.toString(), initialInterval);
+			if(var instanceof Local){
+				Local varLocal = (Local) var;
+				delta.put(varLocal.getName(), initialInterval);
+			}
 		}
 	}
 
 	public boolean equals(Object other) {
 
 		/*
-		 * TODO
-		 * return true iff other and this represent the same domain element, i.e.,
-		 * are defined over the same set of variables and every variable is assigned
-		 * the same interval.
+		 * TODO return true iff other and this represent the same domain
+		 * element, i.e., are defined over the same set of variables and every
+		 * variable is assigned the same interval.
 		 */
-		boolean result = false;
-		if (other instanceof IntervalDomain) {
-			IntervalDomain otherID = (IntervalDomain)other;
-			result = this.delta.equals(otherID.delta);
-		}
-		else {
-			result = false;
-		}
 
-		return result;
+		if (other instanceof IntervalDomain) {
+			IntervalDomain otherID = (IntervalDomain) other;
+			return this.delta.equals(otherID.delta);
+		} else {
+			return false;
+		}
 	}
 
 	public String toString() {
 
 		/*
-		 * TODO
-		 * returns a String representing this interval domain element in the following format:
-		 * VARIABLE_NAME_1:INTERVAL_1 VARIABLE_NAME_2:INTERVAL_2 etc.
-		 * The list of variables has to be ordered lexicographically according to variable names (value.toString()).
-		 * Further, all variable/interval pairs should be separated by exactly one whitespace with no trailing whitespace.
+		 * TODO returns a String representing this interval domain element in
+		 * the following format: VARIABLE_NAME_1:INTERVAL_1
+		 * VARIABLE_NAME_2:INTERVAL_2 etc. The list of variables has to be
+		 * ordered lexicographically according to variable names
+		 * (value.toString()). Further, all variable/interval pairs should be
+		 * separated by exactly one whitespace with no trailing whitespace.
 		 *
-		 * Example:
-		 * a:[-inf,inf] c:[8,19] z:[27:inf]
+		 * Example: a:[-inf,inf] c:[8,19] z:[27:inf]
 		 */
 		String result = "";
-		for (Entry<String,Interval> entry : delta.entrySet()) {
+		for (Entry<String, Interval> entry : delta.entrySet()) {
 			result += " " + entry.getKey() + ":[" + entry.getValue().toString() + "]";
 		}
 		return result.trim();
@@ -69,11 +68,10 @@ public class IntervalDomain {
 	public IntervalDomain lub(IntervalDomain other) {
 
 		/*
-		 * TODO
-		 * return the least upper bound of interval domain elements
-		 * this and other
+		 * TODO return the least upper bound of interval domain elements this
+		 * and other
 		 */
-		BiConsumer<String,Interval> computeLubWithOther = (k,v) -> {
+		BiConsumer<String, Interval> computeLubWithOther = (k, v) -> {
 			Interval i = other.delta.get(k);
 			v = v.lub(i);
 		};
@@ -84,15 +82,13 @@ public class IntervalDomain {
 	public IntervalDomain widen(IntervalDomain other) {
 
 		/*
-		 * TODO
-		 * return the widening of interval domain elements
-		 * this and other
+		 * TODO return the widening of interval domain elements this and other
 		 */
-		BiConsumer<String,Interval> computeWidenWithOther = (k,v) -> {
- 			Interval i = other.delta.get(k);
- 			v = v.widen(i);
- 		};
- 		this.delta.forEach(computeWidenWithOther);
- 		return this;
+		BiConsumer<String, Interval> computeWidenWithOther = (k, v) -> {
+			Interval i = other.delta.get(k);
+			v = v.widen(i);
+		};
+		this.delta.forEach(computeWidenWithOther);
+		return this;
 	}
 }
